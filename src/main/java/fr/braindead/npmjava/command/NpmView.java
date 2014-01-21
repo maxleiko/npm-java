2d1ad20fbad14c279bc169ea2584e3d1f6602e82
Maxime Tricoire <max.tricoire@gmail.com>
2014-01-21 15:44:37 +0100

    refactoring

--- src/main/java/fr/braindead/npmjava/command/NpmView.java
+++ src/main/java/fr/braindead/npmjava/command/NpmView.java
@@ -5,7 +5,6 @@ import com.google.gson.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Map;
@@ -20,26 +19,14 @@ public class NpmView {
     
     public void execute(String pattern, ViewCallback cb) {
         try {
-            URL url = new URL("http://registry.npmjs.org/"+pattern);
-            URLConnection conn = url.openConnection();
-            InputStream is = conn.getInputStream();
-            JsonParser jsonParser = new JsonParser();
-            JsonObject data = (JsonObject) jsonParser.parse(new InputStreamReader(is));
+            JsonObject data = getRemoteData(pattern);
             JsonObject viewResult = new JsonObject();
             
             if (data.has("error")) {
                 cb.onError(new Exception("Unable to find "+pattern+" in npm registry"));
 
             } else {
-                JsonObject versions = data.getAsJsonObject("versions");
-                JsonPrimitive latest = data.getAsJsonObject("dist-tags").getAsJsonPrimitive("latest");
-                JsonArray arrayVersions = new JsonArray();
-                for (Map.Entry<String, JsonElement> entry : versions.entrySet()) {
-                    arrayVersions.add(new JsonPrimitive(entry.getKey()));
-                }
-                viewResult.add("versions", arrayVersions);
-                viewResult.add("version", latest);
-                // TODO add all other information based on "npm view" specification
+                computeViewResult(data, viewResult);
                 cb.onSuccess(viewResult);
             }
             
@@ -48,29 +35,38 @@ public class NpmView {
         }
     }
     
-    public JsonObject execute(String pattern) throws Exception {
-        URL url = new URL("http://registry.npmjs.org/"+pattern);
-        URLConnection conn = url.openConnection();
-        InputStream is = conn.getInputStream();
-        JsonParser jsonParser = new JsonParser();
-        JsonObject data = (JsonObject) jsonParser.parse(new InputStreamReader(is));
+    public JsonObject execute(String pattern) throws Exception {       
+        JsonObject data = getRemoteData(pattern);
         JsonObject viewResult = new JsonObject();
 
         if (data.has("error")) {
             throw new Exception("Unable to find "+pattern+" in npm registry");
 
         } else {
-            JsonObject versions = data.getAsJsonObject("versions");
-            JsonPrimitive latest = data.getAsJsonObject("dist-tags").getAsJsonPrimitive("latest");
-            JsonArray arrayVersions = new JsonArray();
-            for (Map.Entry<String, JsonElement> entry : versions.entrySet()) {
-                arrayVersions.add(new JsonPrimitive(entry.getKey()));
-            }
-            viewResult.add("versions", arrayVersions);
-            viewResult.add("version", latest);
-            // TODO add all other information based on "npm view" specification
+            computeViewResult(data, viewResult);
             return viewResult;
         }
+    }
+    
+    private JsonObject getRemoteData(String pattern) throws IOException {
+        URL url = new URL("http://registry.npmjs.org/"+pattern);
+        URLConnection conn = url.openConnection();
+        InputStream is = conn.getInputStream();
+        JsonParser jsonParser = new JsonParser();
+        JsonObject data = (JsonObject) jsonParser.parse(new InputStreamReader(is));
+        return data;
+    }
+    
+    private void computeViewResult(JsonObject data, JsonObject viewResult) {
+        JsonObject versions = data.getAsJsonObject("versions");
+        JsonPrimitive latest = data.getAsJsonObject("dist-tags").getAsJsonPrimitive("latest");
+        JsonArray arrayVersions = new JsonArray();
+        for (Map.Entry<String, JsonElement> entry : versions.entrySet()) {
+            arrayVersions.add(new JsonPrimitive(entry.getKey()));
+        }
+        viewResult.add("versions", arrayVersions);
+        viewResult.add("version", latest);
+        // TODO add all other information based on "npm view" specification
     }
     
     public interface ViewCallback {
